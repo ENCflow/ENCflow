bb7d6bc839393ce26d20edc5b2ec228770f9bf9a
Tsuyoshi Tada <tadadaze@gmail.com>
2026-07-14 11:59:27 +0900

    並列化：コマンドライン引数処理をMPI初期化後に移動

--- src/m_main.f90
+++ src/m_main.f90
@@ -1,5 +1,4 @@
 module m_main
-  use m_parallel
   use m_sysparam
   use m_geoinfo
   use m_precip
@@ -10,6 +9,7 @@ module m_main
   use m_swflow
   use m_fileio
   use sysdep_util
+  use m_parallel
 
   implicit none
   private
@@ -33,8 +33,7 @@ contains
 !----------------------------------------------------------------------
 ! メインルーチン
 !----------------------------------------------------------------------
-subroutine m_main_all(fn_sysparam)
-  character(len=*), intent(in) :: fn_sysparam
+subroutine m_main_all()
   type(t_sysparam) :: p
   type(t_geoinfo) :: g
   type(t_precip) :: pr
@@ -43,10 +42,14 @@ subroutine m_main_all(fn_sysparam)
   type(t_state) :: s
   type(t_record) :: r
   type(t_swflow) :: sw
+  character(len=256) :: fn_sysparam
   integer :: ierror
 
   ! MPIを初期化
   call par_init()
+
+  ! システムパラメータファイル名を取得
+  call get_fn_param(fn_sysparam)
 
   ! システムを初期化
   call m_sysparam_init(p, fn_sysparam)    ! sysparam を初期化
@@ -340,6 +343,45 @@ subroutine output_matrix_int(p, prefix, a, k)
   end if
   if (p%f_output_mode == 2 .or. p%f_output_mode == 3) then
     call fileio_write_matrix(fn//".bil", p%nx, p%ny, a, e_fmt_bil, p%f_output_compress)
+  end if
+end subroutine
+
+
+!----------------------------------------------------------------------
+! コマンドライン引数から設定ファイル名を取得する
+!----------------------------------------------------------------------
+subroutine get_fn_param(fn_param)
+  character(len=*) :: fn_param
+
+  ! コマンドライン引数の文字列を保存する構造体の宣言
+  !  可変長文字列の配列が直接作れないため構造体の配列を利用
+  type :: arguments
+    character(:), allocatable :: v           ! 無指定文字長（可変長）文字列変数vを要素に持つ
+  end type
+
+  ! コマンドライン引数の数と文字列
+  integer :: argc
+  type(arguments), allocatable :: arg(:)
+
+  
+  !--- コマンドライン引数取得の準備 ---
+  argc = command_argument_count()            ! 引数の数を取得
+  allocate(arg(0:argc))                      ! 構造体のメモリを確保
+
+  !--- コマンドライン引数を取得 ---
+  get_arguments: block
+    integer :: i, l
+    do i = 0, argc
+      call get_command_argument(number=i, length=l)         ! i番目の引数の長さを取得
+      allocate(character(l) :: arg(i)%v)                    ! 構造体中の文字列用メモリを確保
+      call get_command_argument(number=i, value=arg(i)%v)   ! 引数の文字列を取得
+    end do
+  end block get_arguments
+
+  if (argc >= 1) then
+    fn_param = arg(1)%v
+  else
+    call par_stop("usage: "//arg(0)%v//" parameterfile")
   end if
 end subroutine
 
--- src/main.f90
+++ src/main.f90
@@ -1,49 +1,7 @@
 program main
   use m_main, only : m_main_all
   implicit none
-  character(len=256) :: fn_param
 
-  call get_fn_param(fn_param)
-  call m_main_all(fn_param)
-
-contains
-!----------------------------------------------------------------------
-! コマンドライン引数から設定ファイル名を取得する
-!----------------------------------------------------------------------
-subroutine get_fn_param(fn_param)
-  character(len=*) :: fn_param
-
-  ! コマンドライン引数の文字列を保存する構造体の宣言
-  !  可変長文字列の配列が直接作れないため構造体の配列を利用
-  type :: arguments
-    character(:), allocatable :: v           ! 無指定文字長（可変長）文字列変数vを要素に持つ
-  end type
-
-  ! コマンドライン引数の数と文字列
-  integer :: argc
-  type(arguments), allocatable :: arg(:)
-
-  
-  !--- コマンドライン引数取得の準備 ---
-  argc = command_argument_count()            ! 引数の数を取得
-  allocate(arg(0:argc))                      ! 構造体のメモリを確保
-
-  !--- コマンドライン引数を取得 ---
-  get_arguments: block
-    integer :: i, l
-    do i = 0, argc
-      call get_command_argument(number=i, length=l)         ! i番目の引数の長さを取得
-      allocate(character(l) :: arg(i)%v)                    ! 構造体中の文字列用メモリを確保
-      call get_command_argument(number=i, value=arg(i)%v)   ! 引数の文字列を取得
-    end do
-  end block get_arguments
-
-  if (argc >= 1) then
-    fn_param = arg(1)%v
-  else
-    print ('(a, a, a)'), "usage: ", arg(0)%v, " parameterfile"
-    stop
-  end if
-end subroutine
+  call m_main_all()
 
 end program
