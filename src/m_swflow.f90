1d21a70b3d6a868b4e7f193eba61db439886f07b
Tsuyoshi Tada <tadadaze@gmail.com>
2026-07-28 14:01:16 +0900

    m_swflowの手続きポインタをt_swflowの中に移動

--- src/m_swflow.f90
+++ src/m_swflow.f90
@@ -15,21 +15,11 @@ module m_swflow
   public :: m_swflow_dispose
 
 
-  type t_swflow
-    logical :: initialized = .false.
-  end type
-
-
-  ! 地表水計算ルーチンのポインタを宣言しヌルポインタで初期化
-  procedure(procedure_swflow_init), pointer :: swflow_init => null()
-  procedure(procedure_swflow_calc), pointer :: swflow_calc => null()
-  procedure(procedure_swflow_dispose), pointer :: swflow_dispose => null()
-
-
   !-------------------------------------------
   ! 地表水計算ルーチンのインターフェース
+  ! (型成分で参照するため、型定義より前に置くこと)
   !-------------------------------------------
-  interface
+  abstract interface
     subroutine procedure_swflow_init(p, g, b, s)
       use m_sysparam, only : t_sysparam
       use m_geoinfo, only : t_geoinfo
@@ -61,6 +51,17 @@ module m_swflow
   !-------------------------------------------
 
 
+  type t_swflow
+    ! 格子系実装(ENC/STG)への手続きポインタ。インスタンスの実体であり、
+    ! モジュール変数にしないこと(将来のネスト格子等での多重化のため。
+    ! nopass: 呼び出し時に自身を第1引数として渡さない通常の手続き)
+    procedure(procedure_swflow_init),    pointer, nopass :: init    => null()
+    procedure(procedure_swflow_calc),    pointer, nopass :: calc    => null()
+    procedure(procedure_swflow_dispose), pointer, nopass :: dispose => null()
+    logical :: initialized = .false.
+  end type
+
+
 contains
 
 !======================================================================
@@ -77,21 +78,21 @@ subroutine m_swflow_init(sw, p, g, b, s)
   type(t_boundary), intent(in) :: b
   type(t_state), intent(inout) :: s
 
-  ! 地表水計算ルーチンのポインタをセット
+  ! 地表水計算ルーチンのポインタをこのインスタンスに束縛する
   if (p%f_gridsystem == 0) then
-    swflow_init => m_swflow_enc_init
-    swflow_calc => m_swflow_enc_calc
-    swflow_dispose => m_swflow_enc_dispose
+    sw%init    => m_swflow_enc_init
+    sw%calc    => m_swflow_enc_calc
+    sw%dispose => m_swflow_enc_dispose
   else if (p%f_gridsystem == 1) then
-    swflow_init => m_swflow_stg_init
-    swflow_calc => m_swflow_stg_calc
-    swflow_dispose => m_swflow_stg_dispose
+    sw%init    => m_swflow_stg_init
+    sw%calc    => m_swflow_stg_calc
+    sw%dispose => m_swflow_stg_dispose
   else
     call par_stop("invalid gridsystem")
   end if
   sw%initialized = .true.
 
-  call swflow_init(p, g, b, s)
+  call sw%init(p, g, b, s)
 end subroutine
 
 !----------------------------------------------------------------------
@@ -104,8 +105,8 @@ subroutine m_swflow_calc(sw, p, g, b, s, ierror)
   type(t_boundary), intent(in) :: b
   type(t_state), intent(inout) :: s
   integer, intent(inout) :: ierror
-  if (sw%initialized) continue  ! 引数未使用の警告を抑制
-  call swflow_calc(p, g, b, s, ierror)
+  if (.not. sw%initialized) call par_stop("m_swflow_calc: not initialized")
+  call sw%calc(p, g, b, s, ierror)
 end subroutine
 
 
@@ -115,10 +116,10 @@ end subroutine
 subroutine m_swflow_dispose(sw, p)
   type(t_swflow), intent(inout) :: sw
   type(t_sysparam), intent(in) :: p
-  call swflow_dispose(p)
-  swflow_init => null()
-  swflow_calc => null()
-  !swflow_dispose => null()
+  call sw%dispose(p)
+  sw%init    => null()
+  sw%calc    => null()
+  sw%dispose => null()
   sw%initialized = .false.
 end subroutine
 
